7db380ecec9fe0cd44f4bfc40742cd844b1e25b5
Giau <giau.huynh@saigontechnology.com>
2024-03-20 14:08:26 +0700

    table chakra styling

--- create-vite-ts-app/src/components/TablePagination.tsx
+++ create-vite-ts-app/src/components/TablePagination.tsx
@@ -10,6 +10,22 @@ import {
 } from '@tanstack/react-table';
 import fetchData from '../data/fetchData';
 import { Person } from '../models/Person';
+import {
+    Box,
+    Button,
+    Flex,
+    Input,
+    Select,
+    Spacer,
+    Stack,
+    Table,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+    Wrap,
+} from '@chakra-ui/react';
 export default function TablePaginationComponent() {
     const rerender = React.useReducer(() => ({}), {})[1];
 
@@ -97,15 +113,14 @@ export default function TablePaginationComponent() {
     });
 
     return (
-        <div className="p-2">
-            <div className="h-2" />
-            <table>
-                <thead>
+        <Stack direction={'column'}>
+            <Table>
+                <Thead>
                     {table.getHeaderGroups().map((headerGroup) => (
-                        <tr key={headerGroup.id}>
+                        <Tr key={headerGroup.id}>
                             {headerGroup.headers.map((header) => {
                                 return (
-                                    <th
+                                    <Th
                                         key={header.id}
                                         colSpan={header.colSpan}
                                     >
@@ -118,104 +133,115 @@ export default function TablePaginationComponent() {
                                                 )}
                                             </div>
                                         )}
-                                    </th>
+                                    </Th>
                                 );
                             })}
-                        </tr>
+                        </Tr>
                     ))}
-                </thead>
-                <tbody>
+                </Thead>
+                <Tbody>
                     {table.getRowModel().rows.map((row) => {
                         return (
-                            <tr key={row.id}>
+                            <Tr key={row.id}>
                                 {row.getVisibleCells().map((cell) => {
                                     return (
-                                        <td key={cell.id}>
+                                        <Td key={cell.id}>
                                             {flexRender(
                                                 cell.column.columnDef.cell,
                                                 cell.getContext()
                                             )}
-                                        </td>
+                                        </Td>
                                     );
                                 })}
-                            </tr>
+                            </Tr>
                         );
                     })}
-                </tbody>
-            </table>
-            <div className="h-2" />
-            <div className="flex items-center gap-2">
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.firstPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {'<<'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {'<'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {'>'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.lastPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {'>>'}
-                </button>
-                <span className="flex items-center gap-1">
-                    <div>Page</div>
-                    <strong>
-                        {table.getState().pagination.pageIndex + 1} of{' '}
-                        {table.getPageCount().toLocaleString()}
-                    </strong>
-                </span>
-                <span className="flex items-center gap-1">
-                    | Go to page:
-                    <input
-                        type="number"
-                        defaultValue={table.getState().pagination.pageIndex + 1}
-                        onChange={(e) => {
-                            const page = e.target.value
-                                ? Number(e.target.value) - 1
-                                : 0;
-                            table.setPageIndex(page);
-                        }}
-                        className="border p-1 rounded w-16"
-                    />
-                </span>
-                <select
-                    value={table.getState().pagination.pageSize}
-                    onChange={(e) => {
-                        table.setPageSize(Number(e.target.value));
-                    }}
-                >
-                    {[10, 20, 30, 40, 50].map((pageSize) => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
-                {dataQuery.isFetching ? 'Loading...' : null}
-            </div>
-            <div>
-                Showing {table.getRowModel().rows.length.toLocaleString()} of{' '}
-                {dataQuery.data?.rowCount.toLocaleString()} Rows
-            </div>
-            <div>
-                <button onClick={() => rerender()}>Force Rerender</button>
-            </div>
-            <pre>{JSON.stringify(pagination, null, 2)}</pre>
-        </div>
+                </Tbody>
+            </Table>
+            <Flex>
+                <Box height={'inherit'}>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.firstPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<<'}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {'<'}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>'}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.lastPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {'>>'}
+                    </Button>
+                </Box>
+                <Spacer />
+                <Stack direction={'row'}>
+                    <Wrap>
+                        Page
+                        <strong>
+                            {table.getState().pagination.pageIndex + 1} of{' '}
+                            {table.getPageCount().toLocaleString()}
+                        </strong>
+                    </Wrap>
+                    <Wrap>
+                        | Go to page:
+                        <Input
+                            height={'inherit'}
+                            w="16"
+                            type="number"
+                            defaultValue={
+                                table.getState().pagination.pageIndex + 1
+                            }
+                            onChange={(e) => {
+                                const page = e.target.value
+                                    ? Number(e.target.value) - 1
+                                    : 0;
+                                table.setPageIndex(page);
+                            }}
+                        />
+                    </Wrap>
+                    <Wrap>
+                        <Select
+                            height={'inherit'}
+                            w="50"
+                            value={table.getState().pagination.pageSize}
+                            onChange={(e) => {
+                                table.setPageSize(Number(e.target.value));
+                            }}
+                        >
+                            {[10, 20, 30, 40, 50].map((pageSize) => (
+                                <option key={pageSize} value={pageSize}>
+                                    Show {pageSize}
+                                </option>
+                            ))}
+                        </Select>
+                    </Wrap>
+                    {dataQuery.isFetching ? 'Loading...' : null}
+                </Stack>
+                <Spacer />
+                <Box flexDirection={'row-reverse'}>
+                    <div>
+                        Showing{' '}
+                        {table.getRowModel().rows.length.toLocaleString()} of{' '}
+                        {dataQuery.data?.rowCount.toLocaleString()} Rows
+                    </div>
+                </Box>
+            </Flex>
+        </Stack>
     );
 }
